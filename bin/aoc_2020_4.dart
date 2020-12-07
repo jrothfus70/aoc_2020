9bcd2258c5fd34e5d26488c3d762a06f6517bd2e
Jeremy Rothfus <jrothfus@agleader.com>
2020-12-06 21:20:09 -0600

    Day 4 part 2

--- bin/aoc_2020_4.dart
+++ bin/aoc_2020_4.dart
@@ -5,17 +5,47 @@ import 'dart:io';
 class Passport
 {
   Map<String, String> fields;
+  Map<String, Function> field_checks = {};
+  var required_fields = <String>['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
 
   //============================================================================
   Passport( Map<String, String> fields )
   {
     this.fields = fields;
+    field_checks =
+    {
+      'byr' : isBirthYearValid,
+      'iyr' : isIssueYearValid,
+      'eyr' : isExpYearValid,
+      'hgt' : isHeightValid,
+      'hcl' : isHairColorValid,
+      'ecl' : isEyeColorValid,
+      'pid' : isPidValid,
+    };
   }
 
   //============================================================================
-  bool isValid( List<String> keys )
+  bool isValid()
   {
-    for( var key in keys )
+    if ( hasValidFields() )
+    {
+      for( var field in required_fields )
+      {
+        var value = fields[field];
+        if ( !field_checks[field]( value ) )
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+
+  //============================================================================
+  bool hasValidFields()
+  {
+    for( var key in required_fields )
     {
       if( !fields.containsKey( key ) )
       {
@@ -23,6 +53,84 @@ class Passport
       }
     }
     return true;
+  }
+
+  //============================================================================
+  bool isBirthYearValid( String birth_year )
+  {
+    var by = int.parse( birth_year );
+    return by >= 1920 && by <= 2002;
+  }
+
+  //============================================================================
+  bool isIssueYearValid( String issue_year )
+  {
+    var iy = int.parse( issue_year );
+    return iy >= 2010 && iy <= 2020;
+  }
+
+  //============================================================================
+  bool isExpYearValid( String exp_year )
+  {
+    var ey = int.parse( exp_year );
+    return ey >= 2020 && ey <= 2030;
+  }
+
+  //============================================================================
+  bool isHeightValid( String height )
+  {
+    var h = int.parse( (digit().star().flatten()).parse( height ).value );
+    if( height.contains( 'cm' ) )
+    {
+      return h >= 150 && h <= 193;
+    }
+    else if( height.contains( 'in' ) )
+    {
+      return h >= 59 && h <= 76;
+    }
+
+    return false;
+  }
+
+  //============================================================================
+  bool isHairColorValid( String hair_color )
+  {
+    if( hair_color.length == 7 )
+    {
+      if( hair_color[0] == '#' )
+      {
+        var color = hair_color.substring( 1 );
+        final parser = word().star();
+        var matches = parser.parse(color).value;
+        return matches.length == 6;
+      }
+    }
+
+    return false;
+  }
+
+  //============================================================================
+  bool isEyeColorValid( String eye_color )
+  {
+    var colors = <String>[ 'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth' ];
+    
+    for( var color in colors )
+    {
+      if( eye_color.contains( color ) )
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  //============================================================================
+  bool isPidValid( String pass_id )
+  {
+    final parser = digit().star();
+    var matches = parser.parse( pass_id ).value;
+
+    return matches.length == 9;
   }
 }
 
@@ -37,8 +145,7 @@ Map<String, String> getPassportFieldsFromParsed( List<List<dynamic>> parsed )
     var value = field[2].toString();
     fields[key] = value;
   }
- 
-  print( fields );
+
   return fields;
 }
 
@@ -66,11 +173,10 @@ int part1( List<String> passport_db )
 {
   var num_valid = 0;
   var passports = getPassports( passport_db );
-  var keys = <String>['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
 
   for( var passport in passports )
   {
-    if( passport.isValid( keys ) )
+    if( passport.hasValidFields() )
     {
       num_valid++;
     }
@@ -78,6 +184,23 @@ int part1( List<String> passport_db )
 
   return num_valid;
 }
+
+//==============================================================================
+int part2( List<String> passport_db )
+{
+  var num_valid = 0;
+  var passports = getPassports( passport_db );
+
+  for( var passport in passports )
+  {
+    if( passport.isValid() )
+    {
+      num_valid++;
+    }
+  }
+  return num_valid;
+}
+
 
 //==============================================================================
 List<String> readInput( var filename )
@@ -106,14 +229,16 @@ List<String> readInput( var filename )
     passport_data.add( temp );
   }
 
-  print( passport_data );
   return passport_data;
 }
 
 //==============================================================================
 void main()
 {
-  var input1 = readInput( 'input_4.txt' );
-  var num_valid = part1( input1 );
+  var input = readInput( 'input_4.txt' );
+  var num_valid = part1( input );
   print( 'Part 1 result: $num_valid' );
+
+  num_valid = part2( input );
+  print( 'Part 2 result: $num_valid' );
 }
--- test/aoc_2020_4_test.dart
+++ test/aoc_2020_4_test.dart
@@ -7,14 +7,14 @@ void main()
   //============================================================================
   test( 'readInput', () 
   {
-    var data = readInput( 'test_input_4.txt' );
+    var data = readInput( 'test/test_input_4.txt' );
     expect( data.length, 4 );
   });
 
   //============================================================================
   test( 'getPassports', () 
   {
-    var data = readInput( 'test_input_4.txt' );
+    var data = readInput( 'test/test_input_4.txt' );
     var passports = getPassports( data );
     expect( passports.length, 4 );
   });
@@ -22,7 +22,21 @@ void main()
   //============================================================================
   test( 'Part 1', () 
   {
-    var data = readInput( 'test_input_4.txt' );
+    var data = readInput( 'test/test_input_4.txt' );
     expect( part1( data ), 2 );
+  });
+
+  //============================================================================
+  test( 'part 2 invalid passports', ()
+  {
+    var data = readInput( 'test/test_input_4_invalid_passports.txt' );
+    expect( part2( data ), 0 );
+  });
+
+  //============================================================================
+  test( 'part 2 valid passports', ()
+  {
+    var data = readInput( 'test/test_input_4_valid_passports.txt' );
+    expect( part2( data ), 4 );
   });
 }
